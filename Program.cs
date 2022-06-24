ce8c13d9bba2b9eecbdc6ed2c663801530a4e4c8
Emiliya <107909207+marchenkoev@users.noreply.github.com>
2022-06-24 12:42:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int n1, n2;
+
+Console.Write("Введите число 1: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+if(n1 > n2)
+{
+    Console.Write($"max = {n1} min =  {n2}");
+}
+else
+{
+    Console.Write($"max = {n2} min = {n1}");
+}
+
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int n1, n2, n3;
+
+Console.Write("Введите число 1: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 3: ");
+n3 = Convert.ToInt32(Console.ReadLine());
+
+if(n1 > n2)
+{
+    Console.Write(n1);
+}
+else
+if(n2 > n3)
+{
+   Console.Write(n2);  
+}
+else
+{
+     Console.Write(n3);
+}
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+int n1;
+
+Console.Write("Введите число 1: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+if(n1 % 2 == 0)
+{
+    Console.Write("да");
+}
+else
+{
+     Console.Write("нет");
+}
